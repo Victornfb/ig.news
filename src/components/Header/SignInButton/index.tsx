4a3be6612f6a2a9b37f2da5d6a6d8db65bc748c5
Victor Nunes <victornfb@outlook.com>
2022-01-11 12:15:40 -0300

    Initial commit

---
+++ src/components/Header/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+export function SignInButton() {
+    const { data: session } = useSession()
+
+    return session ? (
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut() }
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => { signIn('github') }}
+        >
+            <FaGithub color="#eba417" />
+            Sign In with Github
+        </button>
+    )
+}
